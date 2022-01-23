af9b36472fa3136d677a85dbe9566f2f280de012
Scissordragonboy <Scissordragonboy@gmail.com>
2022-01-23 07:05:52 +0000

    Revert "omni => twrp"
    This reverts commit 5eb5ed2dcae6c350952b9494d3683771669bc693.

--- omni_RMX2076.mk
+++ omni_RMX2076.mk
@@ -14,12 +14,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product, device/realme/RMX2076/device.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
-#$(call inherit-product, vendor/twrp/config/gsm.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
+#$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := RMX2076
-PRODUCT_NAME := twrp_RMX2076
+PRODUCT_NAME := omni_RMX2076
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := RMX2076
 PRODUCT_MANUFACTURER := realme
